9bddacf1b5d0fb5cefcf5deae2bc68431cc75938
Chrisjlan89 <chrisj17@gmail.com>
2018-04-07 12:02:35 -0400

    Revert "conditionally render twitch video"
    This reverts commit 84968ea9f73c1aeb03ebe5838532b6370c5b5f33.

--- client/src/Components/TwitchStream.js
+++ client/src/Components/TwitchStream.js
@@ -5,10 +5,42 @@ const EMBED_URL = 'https://embed.twitch.tv/embed/v1.js';
 
 
 
-const TwitchStream=(props) => {
-  
+    class TwitchStream extends React.Component {
       
- 
+        state = {
+            targetID: 'twitch-embed',
+            width: '940',
+            height: '480',
+            channel: 'UnderBlank'
+          }
+
+          handleInputChange = event => {
+   
+            // deconstruction way
+            //  aka
+            // const name = event.target.name
+            // const value = event.target.value
+            const {name , value} = event.target;
+            console.log('name', name, value)
+            this.setState({
+              [name] : value
+            })
+          }
+          
+          
+
+
+componentDidMount() {
+      API.search('Starcraft')
+            .then(res => { 
+                console.log(res)
+                console.log(res.data)
+                this.setState({  channel: res.data.streams[0].channel.display_name });
+            this.handleVid()
+            })  
+        }
+        
+            handleVid = event => {
         let embed;
         
         const script = document.createElement('script');
@@ -17,30 +49,30 @@ const TwitchStream=(props) => {
           EMBED_URL
         );
         script.addEventListener('load', () => {
-          embed = new window.Twitch.Embed(props.targetID, { ...props});
+          embed = new window.Twitch.Embed(this.state.targetID, { ...this.state });
         });
         document.body.appendChild(script);
     
-    
+    }
 
     
- 
+      render() {
     
         return (
           <div>
-            Hello {props.channel} {props.targetID} {props.width} {props.height}
-            <div id={props.targetID}></div>
+            TwitchStream {this.state.channel}
+            <div id={this.state.targetID}></div>
           </div>
         )
-      
+      }
     }
     
     // TwitchStream.defaultProps = {
-    //    targetID: 'twitch-embed',
-    //    width: '940',
-    //    height: '480',
-    //    channel: 'summit1g',
-    //  }
+    //   targetID: 'twitch-embed',
+    //   width: '940',
+    //   height: '480',
+    //   channel: 'summit1g',
+    // }
     
 
             // search = event => {
