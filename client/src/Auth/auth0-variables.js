c3616298f41ad4bd3fa7022396f15f1269f0901d
stehar14 <stehar14@yahoo.com>
2018-04-19 18:40:13 -0400

    Update auth0-variables.js

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,7 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'gaminggurus.auth0.com',
   clientId: 'rXCMkRaYQ6wxFFBte8c7y0u3Qd8WOFfg',
-  callbackUrl: 'http://localhost:3000/callback'
-  // https://gaminggurus.herokuapp.com/callback
-  // 
+  callbackUrl: 'https://gaminggurus.herokuapp.com/callback'
+  // http://localhost:3000/callback
 }
