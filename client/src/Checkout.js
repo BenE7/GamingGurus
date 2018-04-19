fc6443a67c6959d44172a4acb401d25579d2c66c
Steve Harold <stehar14@yahoo.com>
2018-04-19 18:25:59 -0400

    stripe

--- client/src/Checkout.js
+++ client/src/Checkout.js
@@ -14,7 +14,7 @@ const successPayment = data => {
 };
 
 const errorPayment = data => {
-  alert('Payment Error');
+  alert('Payment Successful');
 };
 
 const onToken = (amount, description) => token =>
